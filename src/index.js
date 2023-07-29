4913286665b05d960995d2321260ccb6708a0323
Artur <110535829+Primus1C@users.noreply.github.com>
2023-07-29 23:35:05 +0200

    develop

--- src/index.js
+++ src/index.js
@@ -1,45 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-//import "./index.css";
+import "./index.css";
+import userData from "./data/user.json";
 
-
-
-const Card = (user) => {
-        return (
-            <div class="profile">
-                <div class="description">
-                    <img
-                        src={user.avatar}
-                        alt="User avatar"
-                        class="avatar"
-                    />
-                    <p class="name">Petra Marica</p>
-                    <p class="tag">@pmarica</p>
-                    <p class="location">Salvador, Brasil</p>
-                </div>
-
-                <ul class="stats">
-                    <li>
-                        <span class="label">Followers</span>
-                        <span class="quantity">1000</span>
-                    </li>
-                    <li>
-                        <span class="label">Views</span>
-                        <span class="quantity">2000</span>
-                    </li>
-                    <li>
-                        <span class="label">Likes</span>
-                        <span class="quantity">3000</span>
-                    </li>
-                </ul>
+const Card = () => {
+    return (
+        <div class="profile">
+            <div class="description">
+                <img src={userData.avatar} alt="User avatar" class="avatar" />
+                <p class="name">{userData.username}</p>
+                <p class="tag">{userData.tag}</p>
+                <p class="location">{userData.location}</p>
             </div>
-        )
-    }
+
+            <ul class="stats">
+                <li>
+                    <span class="label">Followers</span>
+                    <span class="quantity">{userData.stats.followers}</span>
+                </li>
+                <li>
+                    <span class="label">Views</span>
+                    <span class="quantity">{userData.stats.views}</span>
+                </li>
+                <li>
+                    <span class="label">Likes</span>
+                    <span class="quantity">{userData.stats.likes}</span>
+                </li>
+            </ul>
+        </div>
+    );
+};
 
 
-function Profile(user) {
-    return null;
-}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Card/>);
+root.render(<Card />);
