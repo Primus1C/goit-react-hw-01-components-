b838c18f5bf200a8947d82d432d3ef778386010a
Artur <110535829+Primus1C@users.noreply.github.com>
2023-07-29 22:36:17 +0200

    develop

--- src/index.js
+++ src/index.js
@@ -4,6 +4,42 @@ import ReactDOM from "react-dom/client";
 
 
 
+const Card = (user) => {
+        return (
+            <div class="profile">
+                <div class="description">
+                    <img
+                        src={user.avatar}
+                        alt="User avatar"
+                        class="avatar"
+                    />
+                    <p class="name">Petra Marica</p>
+                    <p class="tag">@pmarica</p>
+                    <p class="location">Salvador, Brasil</p>
+                </div>
 
-//const root = ReactDOM.createRoot(document.getElementById("root"));
-//root.render();
+                <ul class="stats">
+                    <li>
+                        <span class="label">Followers</span>
+                        <span class="quantity">1000</span>
+                    </li>
+                    <li>
+                        <span class="label">Views</span>
+                        <span class="quantity">2000</span>
+                    </li>
+                    <li>
+                        <span class="label">Likes</span>
+                        <span class="quantity">3000</span>
+                    </li>
+                </ul>
+            </div>
+        )
+    }
+
+
+function Profile(user) {
+    return null;
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Card/>);
